fa570bf640aea8ef6e40a28b959e07b888cd6d91
Olle Jernström <ollejer@ug.kth.se>
2022-08-09 22:03:42 +0200

    add user context, example usage in news-card component

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ import News from './screens/news';
 import Profile from './screens/profile';
 
 import { TabIcon } from './components'
-
+import UserHandler from './utils/user';
 
 
 
@@ -33,93 +33,95 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { elevation: 0 },
-            cardStyle: { backgroundColor: BLACK },
-          }}
-        >
-
-          <Stack.Screen
-            name="Login"
-            options={{ headerShown: false, animationEnabled: false }}
-            component={Login}
-          />
-
-          <Stack.Screen
-            name="Tab"
-            options={{ headerShown: false, animationEnabled: false }}
+      <UserHandler>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerStyle: { elevation: 0 },
+              cardStyle: { backgroundColor: BLACK },
+            }}
           >
-            {() => (
-              <Tab.Navigator
-                tabBarOptions={{
-                  showLabel: false,
-                }}
-              >
 
-                <Tab.Screen
-                  name="Home"
-                  component={Home}
-                  options={{
-                    tabBarIcon: ({ focused }) => (
-                      <TabIcon
-                        focused={focused}
-                        name="home"
-                        text="Home"
-                      />
-                    ),
+            <Stack.Screen
+              name="Login"
+              options={{ headerShown: false, animationEnabled: false }}
+              component={Login}
+            />
+
+            <Stack.Screen
+              name="Tab"
+              options={{ headerShown: false, animationEnabled: false }}
+            >
+              {() => (
+                <Tab.Navigator
+                  tabBarOptions={{
+                    showLabel: false,
                   }}
-                />
+                >
 
-                <Tab.Screen
-                  name="News"
-                  component={News}
-                  options={{
-                    tabBarIcon: ({ focused }) => (
-                      <TabIcon
-                        focused={focused}
-                        name="newspaper"
-                        text="News"
-                      />
-                    ),
-                  }}
-                />
+                  <Tab.Screen
+                    name="Home"
+                    component={Home}
+                    options={{
+                      tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                          focused={focused}
+                          name="home"
+                          text="Home"
+                        />
+                      ),
+                    }}
+                  />
 
-                <Tab.Screen
-                  name="Calendar"
-                  component={Calendar}
-                  options={{
-                    tabBarIcon: ({ focused }) => (
-                      <TabIcon
-                        focused={focused}
-                        name="calendar"
-                        text="Calendar"
-                      />
-                    ),
-                  }}
-                />
+                  <Tab.Screen
+                    name="News"
+                    component={News}
+                    options={{
+                      tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                          focused={focused}
+                          name="newspaper"
+                          text="News"
+                        />
+                      ),
+                    }}
+                  />
+
+                  <Tab.Screen
+                    name="Calendar"
+                    component={Calendar}
+                    options={{
+                      tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                          focused={focused}
+                          name="calendar"
+                          text="Calendar"
+                        />
+                      ),
+                    }}
+                  />
 
 
 
-                <Tab.Screen
-                  name="Profile"
-                  component={Profile}
-                  options={{
-                    tabBarIcon: ({ focused }) => (
-                      <TabIcon
-                        focused={focused}
-                        name="person"
-                        text="Profile"
-                      />
-                    ),
-                  }}
-                />
-              </Tab.Navigator>
-            )}
-          </Stack.Screen>
-        </Stack.Navigator>
-      </NavigationContainer>
+                  <Tab.Screen
+                    name="Profile"
+                    component={Profile}
+                    options={{
+                      tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                          focused={focused}
+                          name="person"
+                          text="Profile"
+                        />
+                      ),
+                    }}
+                  />
+                </Tab.Navigator>
+              )}
+            </Stack.Screen>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </UserHandler>
     </View >
   );
 }
