63bc78670e511dcbb1b8d8442d337337888d75da
mathiasmagnusson <mathiasmagnussons@gmail.com>
2022-08-09 16:53:38 +0200

    ?

--- App.tsx
+++ App.tsx
@@ -1,30 +1,47 @@
-import { StyleSheet, Button, Text } from "react-native";
-import { auth, RecaptchaVerifier, app } from "./lib/firebase";
+import React, { useRef, useState } from "react";
+import { Button } from "react-native";
+import { PhoneAuthProvider } from "firebase/auth";
+import { FirebaseRecaptchaVerifierModal } from "expo-firebase-recaptcha";
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import firebaseConfig from "./.firebase-config";
+
+if (getApps().length == 0)
+  initializeApp(firebaseConfig);
+
+const app = getApp();
+const auth = getAuth(app);
+
 
 export default function App() {
-  const v = new RecaptchaVerifier("recap", null, app);
-  const f = async () => {
-    try {
-      const res = await auth.signInWithPhoneNumber("+46793488021", v);
-      console.log(res);
-    } catch (err) {
-      console.error(err);
-    }
+  const recaptchaVerifierModalRef = useRef<FirebaseRecaptchaVerifierModal>(null);
+  const [doLogin, setDoLogin] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const login = async () => {
+    if (recaptchaVerifierModalRef.current == null) { return }
+
+    const phoneProvider = new PhoneAuthProvider(auth);
+    const verId = await phoneProvider.verifyPhoneNumber(
+      phoneNumber,
+      recaptchaVerifierModalRef.current,
+    );
   };
+  if (doLogin) {
+    console.log("The default app obviously exist: ", getApp());
+    console.log("Or does it?");
+    return (
+      <>
+        <FirebaseRecaptchaVerifierModal
+          ref={recaptchaVerifierModalRef}
+          firebaseConfig={app.options}
+          title="Big chungus"
+        />
+      </>
+    );
+  }
 
   return (
-    <>
-      <Text className="recap" />
-      <Button title="Bing bong" onPress={() => f} />
-    </>
+    <Button title="Bing bong" onPress={() => setDoLogin(true)} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
