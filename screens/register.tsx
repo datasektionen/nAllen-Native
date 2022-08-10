9370cbf3aa4ec8234eef5549790ce20ec789174c
Tom Rehnström <tomreh@ug.kth.se>
2022-08-10 11:41:30 +0200

    Restyle login and registration

--- screens/register.tsx
+++ screens/register.tsx
@@ -1,7 +1,7 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { View, Text, TouchableOpacity, Button, StyleSheet, TextInput, Pressable } from 'react-native'
 import React, { useState } from 'react'
-import login, { auth } from './login'
+import login, { auth, loginStyles } from './login'
 import { BLACK, CERISE_STRONG, WHITE } from '../assets/style/colors';
 
 const Register = ({ navigation }: any) => {
@@ -21,76 +21,36 @@ const Register = ({ navigation }: any) => {
     };
 
     return (
-        <View style={styles.container}>
-            <Text>REGISTER</Text>
-            <View>
-                <Text style={styles.title}>Email</Text>
-                <TextInput style={styles.textInput} onChangeText={email => setEmail(email)} />
-                <Text style={styles.title}>Password</Text>
-                <TextInput style={styles.textInput} onChangeText={password => setPassword(password)} />
-                <Text style={styles.title}>Re-enter Password</Text>
-                <TextInput style={styles.textInput} onChangeText={rePassword => setRePassword(rePassword)} />
+        <View style={loginStyles.container}>
+            <View style={loginStyles.innerContainer}>
+                <Text style={loginStyles.innerTitle}>REGISTER</Text>
+                <View>
+                    <View style={loginStyles.inputField}>
+                        <Text style={loginStyles.title}>Email</Text>
+                        <TextInput style={loginStyles.textInput} onChangeText={email => setEmail(email)} />
+                    </View>
+                    <View style={loginStyles.inputField}>
+                        <Text style={loginStyles.title}>Password</Text>
+                        <TextInput style={loginStyles.textInput} onChangeText={password => setPassword(password)} />
+                    </View>
+                    <View style={loginStyles.inputField}>
+                        <Text style={loginStyles.title}>Re-enter Password</Text>
+                        <TextInput style={loginStyles.textInput} onChangeText={rePassword => setRePassword(rePassword)} />
+                    </View>
+                </View>
+                
+                <View style={loginStyles.buttonBar}>
+                    <Pressable style={loginStyles.button} onPress={() => handleRegister()} >
+                        <Text style={loginStyles.buttonText}>Register</Text>
+                    </Pressable>
+                    <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                        <Text style={loginStyles.linkText} >Login</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-            <Pressable style={styles.button} onPress={() => handleRegister()} >
-                <Text style={styles.buttonText}>Register</Text>
-            </Pressable>
-
-            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-                <Text style={styles.linkText} >Already have an account? Login here!</Text>
-            </TouchableOpacity>
-
-            <Pressable style={{ marginTop: 100, backgroundColor: BLACK }} onPress={() => navigation.navigate('Tab')}>
-                <Text>Bypass</Text>
-            </Pressable>
-        </View>
+        </View >
     )
 }
 
 export default Register
-const styles = StyleSheet.create({
-    title: {
-        color: BLACK,
-        fontWeight: 'bold',
-        textAlign: 'left',
-    },
-
-    textInput: {
-        color: BLACK,
-        fontSize: 20,
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: BLACK,
-        padding: 5
-    },
-    container: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: WHITE,
-        fontWeight: 'bold',
-        fontSize: 40,
-        marginTop: "40vh",
-        padding: 40,
-        // space between the flex elements
-        flexWrap: 'wrap',
-    },
-
-    button: {
-        backgroundColor: CERISE_STRONG,
-        borderRadius: 5,
-        padding: 10,
-        margin: 10,
-    },
-    buttonText: {
-        color: WHITE,
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-
-    linkText: {
-        // color light blue
-        color: '#00bfff',
-        fontSize: 16,
-    },
-});
 
