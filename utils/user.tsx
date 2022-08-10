2e2b20eed59424c0cbe6b428c1fa3b62d386cd60
Olle Jernström <56885633+ollisco@users.noreply.github.com>
2022-08-10 14:33:13 +0200

    set user and move auth to user context (#11)
    Co-authored-by: Olle Jernström <ollejer@ug.kth.se>

--- utils/user.tsx
+++ utils/user.tsx
@@ -1,11 +1,14 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import firebaseConfig from "../.firebase-config";
 import { View, Text } from 'react-native'
 import React, { useContext, useState } from 'react'
-import { UserCredential } from 'firebase/auth'
+import { getAuth, UserCredential, Auth } from 'firebase/auth'
 
 
 interface User {
     user: UserCredential | null
     setUser: (user: UserCredential) => void | undefined
+    auth: Auth | null
 }
 
 interface Props {
@@ -14,15 +17,23 @@ interface Props {
 
 export const UserContext = React.createContext<User>({
     user: null,
-    setUser: () => { }
+    setUser: () => { },
+    auth: null
+
 })
 
 
 const UserHandler: React.FC<Props> = ({ children }) => {
     const [user, setUser] = useState<UserCredential | null>(null) // TODO: get this from local storage if possible
 
+    if (getApps().length == 0)
+        initializeApp(firebaseConfig);
+
+    const app = getApp();
+    const auth = getAuth(app);
+
     return (
-        <UserContext.Provider value={{ user, setUser }}>
+        <UserContext.Provider value={{ user, setUser, auth }}>
             {children}
         </UserContext.Provider>
     )
